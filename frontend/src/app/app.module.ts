54d4b54c836c08bbd21de972cccdd56ec7792270
Danilova09 <93181066+Danilova09@users.noreply.github.com>
2022-02-12 12:17:39 +0600

    Generation of main components in frontend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,15 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { FlexModule } from '@angular/flex-layout';
+import { NewsComponent } from './home/news/news.component';
+import { NewsItemComponent } from './home/news/news-item/news-item.component';
+import { FormComponent } from './form/form.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsComponent,
+    NewsItemComponent,
+    FormComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    FlexModule
   ],
   providers: [],
   bootstrap: [AppComponent]
