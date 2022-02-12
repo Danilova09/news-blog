5276b95816b2e59e9d8d5e4a09f487a61294be5b
Danilova09 <93181066+Danilova09@users.noreply.github.com>
2022-02-12 12:59:55 +0600

    Adding styles to components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -9,16 +8,24 @@ import { MatButtonModule } from '@angular/material/button';
 import { FlexModule } from '@angular/flex-layout';
 import { NewsComponent } from './home/news/news.component';
 import { NewsItemComponent } from './home/news/news-item/news-item.component';
-import { FormComponent } from './form/form.component';
 import { HomeComponent } from './home/home.component';
+import { FormsModule } from '@angular/forms';
+import { FileInputComponent } from './ui/file-input/file-input.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { FormComponent } from './form/form.component';
+import { NewsDetailsComponent } from './news-details/news-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NewsComponent,
     NewsItemComponent,
+    HomeComponent,
+    FileInputComponent,
     FormComponent,
-    HomeComponent
+    NewsDetailsComponent,
   ],
   imports: [
     BrowserModule,
@@ -26,7 +33,11 @@ import { HomeComponent } from './home/home.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatButtonModule,
-    FlexModule
+    FlexModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
